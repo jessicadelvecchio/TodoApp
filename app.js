9678f00008effee5bbdc0b428a1f958793fd1478
Jessica DelVecchio <hey@jdvdv.com>
2024-07-28 22:55:21 -0700

    code refactor

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// //! retrieve from localStorage if there are items stored
-// const todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
-
-
-// // ! display tasks
-// for (let i = 0; i < todosArray.length; i++) {
-
-// 	let removeBtn = document.createElement('button');
-// 	removeBtn.innerText = 'X';
-
-// 	let newTodo = document.createElement("li");
-
-// 	newTodo.innerText = todosArray[i].task;
-
-// 	newTodo.appendChild(removeBtn);
-// 	todoList.appendChild(newTodo);
-// }
-
 // // ! removing todos from array
 // todoList.addEventListener('click', function (e) {
 // 	if (e.target.tagName === 'LI') { // if click on li, add styles
@@ -48,26 +30,6 @@
 // 	}
 // });
 
-// // ! on submit create newTodo and save to local storage
-// form.addEventListener('submit', function (e) {
-// 	e.preventDefault();
-
-// 	const removeBtn = document.createElement('button');
-// 	removeBtn.innerText = 'X';
-
-// 	const newTodo = document.createElement('li');
-// 	const value = document.getElementById('task').value;
-// 	newTodo.innerText = value;
-
-// 	newTodo.appendChild(removeBtn);
-// 	todoList.appendChild(newTodo);
-
-// 	form.reset();
-
-// 	// //! save to localStorage
-// 	// todosArray.push({ task: value, isCompleted: false });
-// 	// localStorage.setItem("todos", JSON.stringify(todosArray));
-// });
 
 // ! code start
 const todoList = document.querySelector(".todo-list"); // create variable ul where todos will be displayed
@@ -82,8 +44,7 @@ function handleSubmit(e) {
 
 	// create span > li > innertext with form entry value
 	const todoValue = document.querySelector(".todo-entry-input").value;
-	todoArray.push(todoValue); // add each new todo to the todoArray
-	localStorage.setItem("todos", JSON.stringify(todoArray)); // set the todoArray as the value of "todos" as a string in localStorage
+	addToLocalStorage(todoValue); // ***** add new todoValue to localStorage *****
 	const todoListItem = document.createElement("li");
 	const todoListItemText = document.createElement("span");
 	todoListItemText.innerText = todoValue;
@@ -100,17 +61,27 @@ function handleSubmit(e) {
 	const deleteBtn = document.createElement("button");
 	deleteBtn.innerText = "X";
 
-	// add event listener - remove items and update todoArray and localStorage
+	// add event listener to remove clicked li from todos list
 	deleteBtn.addEventListener("click", function () {
 		deleteBtn.parentElement.remove(); //remove the li parent item on click
-
-		for (let i = 0; i < todoArray.length; i++) { //loop through todoArray to find the index of the matching todo and remove it from the todoArray
-			if (todoArray[i] === todoValue) {
-				todoArray.splice(i, 1);
-				break;
-			}
-		}
-		localStorage.setItem("todos", JSON.stringify(todoArray)); // update localStorage
+		removeFromLocalStorage(todoValue); // ***** remove todoValue from localStorage *****
 	});
 	todoListItem.prepend(deleteBtn);
+}
+
+// ! add to localStorage
+function addToLocalStorage(todoValue) {
+	todoArray.push(todoValue); // add each new todo to the todoArray
+	localStorage.setItem("todos", JSON.stringify(todoArray)); // set the todoArray as the value of "todos" as a string in localStorage
+}
+
+// ! remove from localStorage
+function removeFromLocalStorage(todoValue) {
+	for (let i = 0; i < todoArray.length; i++) { //loop through todoArray to find the index of the matching todo and remove it from the todoArray
+		if (todoArray[i] === todoValue) {
+			todoArray.splice(i, 1);
+			break;
+		}
+	}
+	localStorage.setItem("todos", JSON.stringify(todoArray)); // update localStorage
 }
